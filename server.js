72906385252d2555f4379f3f8fac512db76f4231
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-01 22:10:38 -0400

    Modified server.js file with setting up our server and connecting our database

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+// Setting up Express Here
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// Our Routes 
+console.log(require);
+require("./Develop/routes/apiRoutes.js")(app);
+require("./Develop/routes/htmlRoutes.js")(app);
+
+// Starts our server
+
+app.listen(PORT, () => {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:3000 in your browser.",
+        PORT,
+        PORT
+      );
+})

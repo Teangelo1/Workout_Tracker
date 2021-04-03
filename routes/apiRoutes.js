473c921593574e707e2b25281ae3b1741b637881
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-03 18:47:11 -0400

    Modified apiRoutes, currently testing to make sure the routes work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,30 @@
 const app = require("express").Router();
 const Workout = require("../public/workout");
 
-
-app.post("/api/workouts", ({body}, res) => {
-    console.log("workout added")
+// Adding a new workout
+app.post("/api/workout", ({ body }, res) => {
+    console.log("new workout")
     Workout.create(body)
-    .then(dbworkout => {
-        res.json(dbworkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    })
+        .then(dbworkout => {
+            res.json(dbworkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        })
 })
+
+
+app.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+
+        { $push: { exercises: req.body } },
+        { new: true }
+
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});

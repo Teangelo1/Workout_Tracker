54b022c1904b6a0a70099d14bca24ad3f208dd68
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-03 16:54:37 -0400

    created models folder with index.js & workouts.js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,13 @@
-const
+const app = require("express").Router();
+const Workout = require("../public/workout");
+
+
+app.post("/api/workouts", ({body}, res) => {
+    console.log("workout added")
+    Workout.create(body)
+    .then(dbworkout => {
+        res.json(dbworkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})

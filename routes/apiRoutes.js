0d6bfd6e962ce7dcd0e80034bb24f40e92ca5d84
Teangelo Burks <burks.teangelo@gmail.com>
2021-04-07 22:04:16 -0400

    Implemented aggregate function to get all data from the DB and add total duration

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -13,11 +13,23 @@ app.post("/api/workouts", ({ body }, res) => {
 
 // giving us our last workout
 
-
 app.get("/api/workouts", (req, res) => {
-    Workout.find({}).then(dbworkout => {res.json(dbworkout);
-}).catch(err => {res.json(err)})
-});
+    Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" }
+        }
+      }
+    ])
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
 
 // giving us our results
 app.get("/api/workouts/range", (req, res) => {
